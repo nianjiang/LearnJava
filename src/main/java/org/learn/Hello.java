a5e5915829e246cdc9e38f5561a03769dbe1afc4
jnh1983 <nothing123@126.com>
2022-02-12 17:35:54 +0800

    update from Mac.

--- src/main/java/org/learn/Hello.java
+++ src/main/java/org/learn/Hello.java
@@ -3,7 +3,7 @@ package org.learn;
 import java.util.Date;
 
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello: \t" + new Date());
+    public static void main(String[] args) {
+        System.out.println("Jdk8: \t" + new Date());
     }
 }
